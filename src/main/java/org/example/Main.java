876aaccf27d1f9043d7237932f2f408a390cb764
baleasarah <125876722+baleasarah@users.noreply.github.com>
2023-03-02 11:48:52 +0200

    am apelat metoda mananca

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,8 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         Animal a= new Animal();
+        Pisica p= new Pisica();
         a.mananca();
+        p.mananca();
     }
 }
